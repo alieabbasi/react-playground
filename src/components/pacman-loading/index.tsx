53e2a4224914eee1bbb7433f399053177366c111
Ali Abbasi <aliabac7887@gmail.com>
2026-05-20 18:25:19 +0330

    feat: add loading dots animations

--- src/components/pacman-loading/index.tsx
+++ src/components/pacman-loading/index.tsx
@@ -1,10 +1,19 @@
 import type { FC } from "react";
 import Pacman from "./components/pacman/Pacman";
 
+import "./styles.css";
+
 const PacmanLoading: FC = () => {
   return (
-    <div className="pacman-laoding-container">
+    <div className="pacman-loading-container">
       <Pacman />
+      <div className="dot dot-1"></div>
+      <div className="dot dot-2"></div>
+      <div className="dot dot-3"></div>
+      <div className="dot dot-4"></div>
+      <div className="dot dot-5"></div>
+      <div className="dot dot-6"></div>
+      <div className="dot dot-7"></div>
     </div>
   );
 };
